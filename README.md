d084ca6ce05ec8da57847459848edf58dd8249c2
lgvv <lgw9898@naver.com>
2022-06-20 13:42:23 +0900

    [ADD] added package

---
+++ README.md
@@ -0,0 +1,3 @@
+# ReferenceKit
+
+A description of this package.
---
+++ Tests/ReferenceKitTests/ReferenceKitTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import ReferenceKit
+
+final class ReferenceKitTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(ReferenceKit().text, "Hello, World!")
+    }
+}
