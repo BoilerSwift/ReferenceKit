9281b414801e2e25f0bb12120a4b31a05a37fe12
lgvv <lgw9898@naver.com>
2022-06-29 02:13:59 +0900

    [추가] ReferenceKitExample을 추가합니다.

--- Examples/ReferenceKitExamples/ViewController.swift
+++ Examples/ReferenceKitExamples/ViewController.swift
@@ -11,35 +11,44 @@ import SwiftUI
 
 class ViewController: UIViewController {
     
-    let button: UIButton {
-       let button = UIButton()
+    var button: UIButton = {
+        let button = UIButton()
         button.setTitle("Go To OpenSourceView", for: .normal)
-        return
+        
+        return button
     }()
+    
+    
     let items: [OpenSourceItem] = [
-        OpenSourceItem("https://github.com/BoilerSwift/ReferenceKit"),
-        OpenSourceItem("https://github.com/KU-Stacks/kuring-lite-ios", isDeprecated: true),
-        OpenSourceItem("https://github.com/KU-Stacks/kuring-sdk-ios-spm", isDeprecated: true),
+        OpenSourceItem(title: "ReferenceKit", url: "https://github.com/BoilerSwift/ReferenceKit"),
+        OpenSourceItem(title: "🐻‍❄️ 만든이", url: "https://rldd.tistory.com"),
+        OpenSourceItem(title: "ReferenceKit", url: "https://github.com/BoilerSwift/ReferenceKit", deprecated: true),
+        OpenSourceItem(title: "🐻‍❄️ 만든이", url: "https://rldd.tistory.com", deprecated: true)
     ]
-    let option: OpenSourceOption = .init()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .gray
         
-        view.backgroundColor = .red
         setupLayout()
-        
+        button.addTarget(self, action: #selector(didTapTouch), for: .touchUpInside)
     }
     
     func setupLayout() {
         view.addSubview(button)
-        
         button.translatesAutoresizingMaskIntoConstraints = false
+        
         NSLayoutConstraint.activate([
-            button.widthAnchor.constraint(equalToConstant: 250),
-            button.heightAnchor.constraint(equalToConstant: 100),
-            button.centerXAnchor.constraint(equalTo: super.view.centerXAnchor),
-            button.centerYAnchor.constraint(equalTo: super.view.centerYAnchor)
+            button.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            button.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            button.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            button.heightAnchor.constraint(equalToConstant: 20),
         ])
+    }
+    
+    @objc
+    func didTapTouch() {
+        let vc = UIHostingController(rootView: OpenSourceView(items: items))
+        navigationController?.pushViewController(vc, animated: true)
     }
 }
